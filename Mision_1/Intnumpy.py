ca098721234a0fdab515eb94742eb02aa4c7a2d9
Mather-v <bynpc04@gmail.com>
2025-11-06 11:49:50 -0500

    file  style.css - Intnumpay.py modified - add library jupyter- book into requirements

--- Mision_1/Intnumpy.py
+++ Mision_1/Intnumpy.py
@@ -1,5 +1,8 @@
-# importar librerias  
-import numpy as np 
-# Array cuyos valores son todo ceros
-a = np.zeros((3,5)) 
+# importar librerias
+import numpy as np
+#Array cuyos valores son todos cero
+a=np.zeros((3,5))
 print(a)
+print(a.ndim)
+print(a.shape)
+print(a.size)
